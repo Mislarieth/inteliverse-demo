5e1ebc3dd79eeb258f38ccf1a3030312d4b1a3e1
Tony <tony@keysmashstudios.com>
2018-02-03 18:38:10 -0500

    changed over to functional basis, will soon put all in rules object

--- Prometheus/PrometheusHost.js
+++ Prometheus/PrometheusHost.js
@@ -12,17 +12,32 @@ var hash=function(obj){
   return hashbase.digest('hex');
 }
 
-//TODO: provide external loading of rules
-//var contract = require('./Contract.js');
+var genid=function(){
+  return hash(Math.random());
+}
+
+var gencommit=function(request){
+
+}
+
+
+var processcommit=function(commit){
+  commit={
+    request:{
+      function: "", //name of function being applied
+      inputvars: {}, //all the vars required for function
+    },
+    signatures:{
+      signers:[],//all the signers
+      signedversion:[] //proof of signature, decrypt with asymetrical key provided in signers (map index to index) and it will equal version
+    }
+  }
+}
+
 
 var metadata={
   blockid:"25kdksl",  //permanent id of the block
   version:"dklsl"     //DAG hash that is result of the hash of previous version with current keys
-  //TODO: host data
-  /*hosts:{
-    hosts:["h2ksl3","dk3ls","hk3ls"],   //impermanent list of hosts
-    hostlocations:["127.0.0.1:8080","127.0.0.1:8081","127.0.0.1:8082"]  //impermanent list of hosts' locations, index maps to index
-  }*/
 }
 
 var data = {
@@ -40,24 +55,63 @@ var data = {
   }
 }
 
-//TODO: commits
-/*var commitdata={
-  key1:[
-    version:"5kskl3",
-    request:{
-      //holds all the data of a request object
-      //used to verify that the request was properly made
-      //also provides access to request variables and everything needed to authenticate request
-      payload:{
-        value:"jimmy"
-      }
-    }
-    signatures: {
-      hosts:["dk3ls","hk3ls"],
-      signedversion:["n3lsk5","n3kls"]
-    }
-  ]
-}*/
+var processrequest=function(funky,inputvars){
+  //do we have the function?
+  if(rules[funky]){
+    //insert into function
+    inputvars=JSON.parse(inputvars);
+    rules[funky](inputvars);
+  }else{
+    return "Function" + funky + "is not in the rules";
+  }
+}
+
+var rules={};
+
+var addrow = function(value){
+  //add a new row to the block
+
+  var rowid = genid();
+
+  data[rowid]={
+    version:hash(value),
+    value:value
+  }
+  metadata.version=hash(hash(metadata.version)+hash(Object.keys(data)));
+  return data[rowid];
+}
+
+var deleterow = function(rowid){
+  //remove an existing row from the block
+  if(data[rowid]){
+    delete data[rowid];
+    metadata.version=hash(hash(metadata.version)+hash(Object.keys(data)));
+
+    return data;
+  }else{
+    return "Row "+rowid+" does not exist";
+  }
+}
+
+var changerow = function(rowid,value){
+  //change a row's value
+  if(data[rowid]){
+    var olddata=data[rowid];
+    var newversion=hash(hash(olddata.version)+hash(value));
+
+    data[rowid].version=newversion;
+    data[rowid].value=value;
+
+
+    return data[rowid];
+  }else{
+
+    return "Row "+rowid+" does not exist";
+  }
+}
+rules.addrow=addrow;
+rules.deleterow=deleterow;
+rules.changerow=changerow;
 
 
 
@@ -84,7 +138,6 @@ server.route({
 })
 
 server.route({
-    //add a new row to the block
     method: 'POST',
     path:'/invoke/addrow',
     handler: function (request, reply) {
@@ -96,22 +149,14 @@ server.route({
         }
         //TODO: generate commit to send out
 
-        var rowid = hash(Math.random());
-        //rowid=JSON.stringify(rowid);
+        result=addrow(value);
 
-        data[rowid]={
-          version:hash(value),
-          value:value
-        }
-        metadata.version=hash(hash(metadata.version)+hash(Object.keys(data)));
-
-        reply(data[rowid]);
+        reply(result);
 
     }
 });
 
 server.route({
-    //remove and existing row from the block
     method: 'POST',
     path:'/invoke/deleterow',
     handler: function (request, reply) {
@@ -122,22 +167,14 @@ server.route({
           return reply("Please provide a rowid");
         }
         //TODO: generate commit to send out
-
-        if(data[rowid]){
-          delete data[rowid];
-          metadata.version=hash(hash(metadata.version)+hash(Object.keys(data)));
-
-          reply(data);
-        }else{
-          reply("Row "+rowid+" does not exist");
-        }
+        result=deleterow(rowid);
+        reply(result);
 
 
     }
 });
 
 server.route({
-    //change a row's value
     method: 'POST',
     path:'/invoke/changerow',
     handler: function (request, reply) {
@@ -154,48 +191,22 @@ server.route({
 
         //TODO: generate commit to send out
 
-        //set the row value
-        if(data[rowid]){
-          var olddata=data[rowid];
-          var newversion=hash(hash(olddata.version)+hash(value));
-
-          data[rowid].version=newversion;
-          data[rowid].value=value;
-
-
-          reply(data[rowid]);
-        }else{
-
-          reply("Row "+rowid+" does not exist");
-        }
+        result=changerow(rowid,value);
+        reply(result);
 
 
     }
 });
 
-/*server.route({
+server.route({
   method: 'POST',
-    path:'/invoke/{funcname}',
-    handler: function (request, reply) {
-      console.log(request.params.funcname);
-        var variables=request.payload;
-        if(variables){
-          var keys=Object.keys(variables);
+  path:'/invoke',
+  handler:function(request,reply){
+    var funky=request.payload.function;
+    var inputvars=request.payload.inputvars;
 
-
-
-          var vars=[];
-          for(key in keys){
-            vars.push(variables[keys[key]]);
-          }
-          console.log(vars);
-        }else{
-          variables=[];
-        }
-        reply(contract[request.params.funcname].apply(null,Array.prototype.slice.call(variables,1)));
-
-    }
-})*/
+  }
+})
 
 server.start((err) => {
 
@@ -204,185 +215,3 @@ server.start((err) => {
     }
     console.log('Server running at:', server.info.uri);
 });
-
-
-/*
-var crypto = require('crypto');
-const Hapi = require('hapi');
-const server = new Hapi.Server();
-
-
-var hash=function(obj){
-  const hashbase = crypto.createHash('sha256');
-  hashbase.update(JSON.stringify(obj));
-  return hashbase.digest('hex');
-}
-
-var maptoobject=function(map){
-  var dataobject={}
-  map.forEach(function(key,value,m){
-    console.log(key);
-    console.log(value);
-    dataobject[key]=value;
-    console.log(dataobject);
-  });
-  return dataobject;
-}
-
-var objecttomap=function(object){
-  var map=new Map();
-  var keys=Object.keys(object);
-  var key="";
-  for(i in keys){
-    key=keys[i];
-    map.set(key,object[key]);
-  }
-  return map;
-}
-
-
-var metadata={
-  blockid:"25kdksl",  //permanent id of the block
-  version:"dklsl"     //DAG hash that is result of the hash of previous version with current keys
-}
-
-var data = new Map(); //map to hold data
-data.set("key1", {  //permanent id of row
-  version:"ad5k3l",   //DAG hash that is a result of the hash of the previous version with the present data
-  value:"meow"    //current value that
-});
-data.set("key2",{
-  version:"kd2ls",
-  value:"hamster"
-});
-data.set("key3",{
-  version: "k3k4d",
-  value: "mreow"
-});
-console.log(data);
-
-
-
-
-server.connection({
-    port: 8080
-});
-
-server.route({
-  //literally just reply the data
-  method: 'GET',
-  path: '/invoke/getdata/',
-  handler: function(request,reply){
-    reply(maptoobject(data));
-  }
-})
-
-server.route({
-  //literally just reply the metadata
-  method: 'GET',
-  path: '/invoke/getmetadata/',
-  handler: function(request,reply){
-    reply(metadata)
-  }
-})
-
-server.route({
-    //add a new row to the block
-    method: 'POST',
-    path:'/invoke/addrow',
-    handler: function (request, reply) {
-        var result;
-        var value = request.payload.value; //could be null
-
-        if(!value){
-          return reply("Please provide a value");
-        }
-        //TODO: generate commit to send out
-
-        var rowid = hash(Math.random());
-
-        data.set(rowid,{
-          version:hash(value),
-          value:value
-        })
-        metadata.version=hash(hash(metadata.version)+hash(data.keys()));
-
-        reply(data.get(rowid));
-
-    }
-});
-
-server.route({
-    //remove and existing row from the block
-    method: 'POST',
-    path:'/invoke/deleterow',
-    handler: function (request, reply) {
-        var result;
-        var rowid = request.payload.rowid; //could be null
-
-        if(!rowid){
-          return reply("Please provide a rowid");
-        }
-        //TODO: generate commit to send out
-
-        if(data.get(rowid)){
-          data.delete(rowid);
-          metadata.version=hash(hash(metadata.version)+hash(data.keys()));
-
-          reply(data);
-        }else{
-          reply("Row "+rowid+" does not exist");
-        }
-
-
-    }
-});
-
-server.route({
-    //change a row's value
-    method: 'POST',
-    path:'/invoke/changerow',
-    handler: function (request, reply) {
-        var result;
-        var rowid = request.payload.rowid; //id of row to change value of
-        var value = request.payload.value; //value to change row to
-
-        if(!rowid){
-          return reply("Please provide a rowid")
-        }
-        if(!value){
-          return reply("Please provide a value")
-        }
-
-        //TODO: generate commit to send out
-
-        //set the row value
-        if(data.get(rowid)){
-          var olddata=data.get(rowid);
-          var newversion=hash(hash(olddata.version)+hash(value));
-
-          var newobject={}
-          newobject.version=newversion;
-          newobject.value=value;
-          data.set(rowid,newobject);
-
-
-          reply(data.get(rowid));
-        }else{
-
-          reply("Row "+rowid+" does not exist");
-        }
-
-
-    }
-});
-
-
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
-*/
